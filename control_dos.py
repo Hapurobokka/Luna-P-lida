e3883ef55f4b085eca1aa8c1b85c2685a2ec660a
Hapurobokka <flaco.legos@gmail.com>
2023-11-24 19:03:40 -0600

    Cambios a la impresion

--- control_dos.py
+++ control_dos.py
@@ -111,29 +111,36 @@ def inspecciones_lab(cadena, inventario):
 
 
 def recolecciones_lab(cadena, inventario):
+	file = open("descripcion_dos_2.txt", "r")
 	if posicion == "centro":
 		if "monoculo" in cadena and "Monoculo raro" in objetos_lab:
-			print(l_texto["Descripcion monoculo"])
+			imprimir_descripcion(file, 28, 6)
 			inventario.append("Monoculo raro")
 			objetos_lab.remove("Monoculo raro")
 		else:
 			print("Eso ya lo recogiste")
 
+	file.close()
+
 
 def descifrar_monoculo(cadena, inventario):
+	file = open("descripcion_dos_2.txt", "r")	
 	if not ("Monoculo raro" in inventario):
 		print("¿Y como vas a hacer eso?")
 		return
 
 	match cadena:
 		case "inscripcion" if posicion == "centro":
-			print(l_texto["Inscripcion puerta"])
+			imprimir_descripcion(file, 931, 7)
 		case "papeles" if posicion == "tercera":
-			print(l_texto["Texto papeles"])
+			imprimir_descripcion(file, 1184, 9)
 		case _:
 			print("¿Qué estas tratando de descifrar?")
 
+	file.close()
+
 def colocar_estatuilla(cadena, inventario):
+	file = open("descripcion_dos_2.txt", "r")
 	if not "Estatuilla" in cadena:
 		print("Parece que el pedestal necesita algo mas")
 		return
@@ -143,11 +150,16 @@ def colocar_estatuilla(cadena, inventario):
 
 	match posicion:
 		case "segunda":
-			print(l_texto["Vision estatuilla uno"])
+			imprimir_descripcion(file, 1712, 13)
 		case "tercera":
-			print(l_texto["Vision estatuilla dos"])
+			imprimir_descripcion(file, 2400, 12)
+
+	file.close()
 
 def miscelaneos_lab(clave, cadena, inventario):
+	file = open("descripcion_dos_2.txt", "r")
+	global maldicion_collar
+
 	match clave:
 		case "descifrar":
 			descifrar_monoculo(cadena, inventario)
@@ -158,35 +170,42 @@ def miscelaneos_lab(clave, cadena, inventario):
 		case "colocar":
 			colocar_estatuilla(cadena, inventario)
 		case "ponerse" if cadena == "colgante":
-			print("No implementado")
-			pass
+			imprimir_descripcion(file, 4779, 4)
+			maldicion_collar = True
 		case _:
 			print("¿Qué tratas de hacer?")
 
+	file.close()
+
 
 def inspecciones_buceo(cadena_b, inventario):
+	file = open("descripcion_dos_2.txt", "r")
 	match cadena_b:
 		case "coral":
-			print(l_texto["Buceo coral"])
+			imprimir_descripcion(file, 3536, 6)
 		case "tablilla" | "tablillas":
-			print(l_texto["Buceo tablillas"])
+			imprimir_descripcion(file, 3800, 10)
 			tablillas(inventario)
 		case "cadaver":
-			print(l_texto["Buceo cadaver"])
+			imprimir_descripcion(file, 4113, 6)
 		case "dorado":
-			print(l_texto["Buceo dorado"])
+			imprimir_descripcion(file, 4511, 6)
 		case _:
 			print("¿Qué estas mirando?")
 
+	file.close()
+
 def recolecciones_buceo(cadena_b, inventario):
+	file = open("descripcion_dos_2.txt", "r")
 	if cadena_b == "colgante":
-		print(l_texto["Descripcion colgante"])
+		imprimir_descripcion(file, 412, 5)
 		inventario.append("Colgante con forma de ojo")
 		objetos_lab.remove("Colgante con forma de ojo")
 	else:
 		print("¿Qué tratas de recoger?")
 
 def apertura_dorado(cadena_b, inventario):
+	file = open("descripcion_dos_2.txt", "r")
 	if not cadena_b == "dorado":
 		print("¿Qué tratas de abrir?")
 		return
@@ -197,28 +216,30 @@ def apertura_dorado(cadena_b, inventario):
 		print("¿Como lo vas a abrir?")
 		return
 
-	print(l_texto["Descripcion emblema calavera"])
+	imprimir_descripcion(file, 809, 3)
 	inventario.append("Emblema calavera")
 	objetos_lab.remove("Emblema calavera")
 	inventario.remove("Llave calavera")
 
 def tablillas(inventario):
+	file = open("descripcion_dos_2.txt", "r")
 	if not "Llave calavera" in objetos_lab:
 		print("Ya tienes la llave, ¿qué mas quieres?")
 		return
 
 	comb = input("Introduce la combinación\n>   ")
 	if comb == tablilla_comb:
-		print(l_texto["Descripcion llave"])
+		imprimir_descripcion(file, 604, 4)
 		inventario.append("Llave calavera")
 		objetos_lab.remove("Llave calavera")
 	else:
 		print("No paso nada. Tal vez te equivocaste.")
 
 def bucear(inventario):
+	file = open("descripcion_dos_2.txt", "r")
 	oxigeno = 3
 
-	print(l_texto["Inicio buceo"])
+	imprimir_descripcion(file, 3080, 11)
 
 	while True:
 		accion = input("> ")
@@ -247,8 +268,11 @@ def bucear(inventario):
 			print("¡Necesitas repirar! Sales inmediatamente a la superficie\n.")
 			break
 
+	file.close()
+
 
 laberinto_terminado = False
+maldicion_collar = False
 
 posicion = "centro"
 
@@ -296,142 +320,4 @@ El silencio sepulcral de unas inmensas ruinas te saluda.
 	"Corto santuario": """
 El rumor relajante del agua te indica que puedes bajar tu guardia un momento.
 	""",
-
-	"Descripcion monoculo": """
-Jamas habias tenido algo tan elegante en tus manos. O bueno, no de esta manera.
-Te cargaste a un riquillo o dos a lo largo de tus años en altamar, arrancando
-de sus lisas y elegantes manos cosas igual de valiosas que estas.
-
-Sin embargo, las circunstancias son diferentes.
-
-Este monoculo puede que marque la diferencia entre la vida y la muerte.
-	""",
-
-	"Descripcion colgante": """
-Jurarias haber visto la forma de ese ojo antes...
-
-¿Por qué subitamente todo esto se te hace tan familiar?
-
-Un poco perturbado, te llevas el collar contigo.
-	""",
-
-	"Descripcion llave": """
-Una de las tablillas se abre como una pequeña puerta, revelando lo que parece
-ser una muy cuidada llave con forma de calavera.
-
-¿Quizás abre algo importante?
-	""",
-
-	"Descripcion emblema calavera": """
-Un emblema con forma de calavera.
-
-Tal vez sirva para activar un mecanismo importante...
-	""",
-
-	"Inscripcion puerta": """
-A traves del monoculo raro, puedes descifrar la inscripción de la puerta:
-
-"Para abrir la puerta, debes encontrar el simbolo que representa el equilibrio
-de la vida y la muerte."
-
-Ahora tienes una pista de que debes hacer.
-	""",
-
-	"Texto papeles": """
-A traves del monoculo raro, el texto se vuelve comprensible:
-
-"He fallado. A todas. No fui capaz de encontrarla. Ni siquiera las paredes de
-este divino lugar pueden aliviar el peso que ahora se asienta en mi corazón.
-La magia de este lugar, siento que me esta llamando. En algún momento vendra
-por mi. Si alguien llega a encontrar mi cuerpo o este escrito, por favor
-vayase de aqui, porque ni siquiera yo, que me considero una experta en el
-HASAF RACTA, se que terminara pasando conmigo."
-	""",
-
-	"Vision estatuilla uno": """
-La tormenta lo esta devorando todo. Sus poderosos vientos arrancan las casas
-desde sus cimientos como si fueran hojas de arboles que salen volando ante la
-menor brisa. Tus hermanas siguen exigiendote que las acompañes para buscar un
-refugio, pero incluso en una situación de desastre, no puede abandonar tu
-oficio. 
-
-Como suma sacerdotisa del HASAF, tienes que velar por la seguridad de madre, y
-es por eso que incluso con el huracan debes encontrarla. Te diriges al interior
-del gran templo, y entonces...
-
-Despiertas despues de ese potente trance. ¿Qué fue eso? Sin embargo, se sintio
-demasiado cercano. Demasiado real... Recoges la estatuilla.
-	""",
-
-	"Vision estatuilla dos": """
-La pequeña corona hecha de conchas marinas es colocada ceremonialmente en tu
-cabeza por la antigua suma sacerdotisa de la aldea. Por el ritual de la RACTA
-ahora tu seras su sucesora. Te arrodillas ante ella, y comienzas a recitar las
-palabras que desde tu niñez tienes grabadas a fuego en tu memoria. 
-
-Hoy te conviertes en la mujer mas importante de la aldea, con excepción de
-Madre, y ahora tienes que cuidar a todas tus hermanas como tu destino siempre
-te ha indicado.
-
-¿Pero estas satisfecha con eso?
-
-Despiertas despues de ese potente trance. ¿Qué fue eso? Sin embargo, se sintio
-demasiado cercano. Demasiado real... Recoges la estatuilla.
-	""",
-
-	"Inicio buceo": """
-El agua es extremadamente fria al tacto, y resulta que si es mucho mas profunda
-de lo que esperabas. 
-
-Un extraño CORAL azulado ilumina la fosa, lo suficiente como para que puedas
-dislumbrar
-
-Una extraño par de TABLILLAS reposa en el fondo, junto con lo que parece ser
-otro CADAVER (¿por qué hay tantos?).
-
-Y finalmente, algo DORADO resplandece en la pared de la izquierda. Se ve que
-esta incrustado ahí. ¿Qué podría ser?
-""",
-
-	"Buceo coral": """
-El coral brilla con la misma intensidad que el gran cristal de la habitación
-central. Ese azul hipnotico, mistico, magico.
-
-Recuerdas a tu capitan sujetando un pedazo de roca similar al coral. 
-
-¿Acaso el sabia algo de este lugar?
-	""",
-
-	"Buceo tablillas": """
-Dos tablillas de piedra descansan hasta el fondo de la fosa.
-
-H B W S F   I R A M S
-F A O S M   M S L A S
-S F W N M   C F W S E
-D A M L C   N W E T N
-C O I F N   L X A B Z
-
-Parece que puedes pulsar algunos de los simbolos, uno por cada fila.
-Podrias intentar escribir algo con ellas.
-	""",
-
-	"Buceo cadaver": """
-Otro cadaver de una mujer, esta vez esta viscoso y cubierto de algas.
-Los trozos de tela adheridos a sus huesos ya no pueden ser considerados
-ropa, y faltan muchos huesos de su torax y parte inferior.
-
-Sin embargo, posee un objeto que no ha decaido como los demas. Un COLGANTE
-con forma de ojo refleja la luz del coral.
-
-Tal vez sea otro artefacto que deberias RECOGER.
-	""",
-
-	"Buceo dorado": """
-Una placa dorada esta incrustada en la pared de la fosa. Sus hermosos relieves
-hechos en oro no parecen haber envejecido un solo día.
-
-Notas una pequeña ranura para insertar algo en ella. ¿Quizás una llave?
-
-Tal vez se pueda ABRIR.
-	"""
 }
